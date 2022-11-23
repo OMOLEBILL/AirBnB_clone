27dce523786d848306fecf1e9fa428e2cf7e8d4e
OMOLEBILL <omolebill01@gmail.com>
2022-11-23 09:44:08 +0000

    bugfix

--- models/base_model.py
+++ models/base_model.py
@@ -17,11 +17,10 @@ class BaseModel:
                             kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
                     self.__dict__[key] = kwargs[key]
-
-        """ a class constructor"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """prints [<class name>] (<self.id>) <self.__dict__>
