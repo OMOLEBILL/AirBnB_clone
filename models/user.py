5b8b828581c2b2de14338d465307bd29496af713
kamau2022 <benkabenson@gmail.com>
2022-11-25 10:22:35 +0000

    user.py module

---
+++ models/user.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""this module inherits from BaseModel class
+"""
+from models import storage
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """a class User that inherits from BaseModel
+    
+     Attributes:
+         email (str): public class attribute for User's email
+         password (str):public class attribute for User's password
+         first_name (str): public class attribute for User's first_name
+         last_name (str): public class attribute for User's last_name
+    """
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
+    def __init__(self, *args, **kwargs):
+        """init method for User class
+             
+        Attributes:
+            args: list with arguments
+            kwargs: a dictionary with arguments
+        """
+        super().__init__(*args, **kwargs)
+   
