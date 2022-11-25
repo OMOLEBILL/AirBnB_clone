8bbde3c8201d437f28b29186341a4b65ae95df7f
kamau2022 <benkabenson@gmail.com>
2022-11-25 16:07:02 +0000

    more classes

---
+++ models/state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""this module inherits from BaseModel class
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """a class State that inherits from BaseModel
+
+    Attribute:
+        name (str): public class attribute for State's name
+    """
+    name = ''
+
+    def __init__(self, *args, **kwargs):
+        """init method for Name class
+
+         Attributes:
+        args: list with arguments
+        kwargs: a dictionary with arguments
+        """
+        super().__init__(*args, **kwargs)
